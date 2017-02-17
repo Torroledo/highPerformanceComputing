01ffc3f685c1fa0c76a3f92297d72d32edc4b7a9
Iván Torroledo Peña <Torroledo@MacBook-Pro-de-Ivan-Torroledo-Pena.local>
2017-02-16 23:18:33 -0500

    write statement

--- benchmark.f90
+++ benchmark.f90
@@ -2,7 +2,7 @@ program plate
 
 	implicit none 
 	
-	integer, parameter :: n = 1000
+	integer, parameter :: n = 100
 	integer :: i,j, iter, half, R, max_iter = 3000
 	
 	logical :: interior(n,n)
@@ -12,8 +12,8 @@ program plate
 	
 	half = (1+n)/2
 	R = .8 * half
-	! Grid initialization 	
 	
+	! Grid initialization 	
 	forall(i=1:n,j=1:n) x(i,j) = i 
 	forall(i=1:n,j=1:n) x(i,j) = j
 	
@@ -25,9 +25,13 @@ program plate
 	
 	grid = 0.
 	where (y > half) grid = 100.
+	
+	! Write Inicial Data
+	open(unit = 1, file = 'initialData.txt')
+	write(1,*) (grid(i,:), i=1,n) 
+	close(1)
 		
 	! Iteration 
-	
 	do iter = 1,max_iter 
 		where (interior)
 			grid = (1/4) * 	(cshift(grid, dim=1, shift=1) + &
@@ -37,4 +41,9 @@ program plate
 		end where			 
 		write(*,*) iter
 	end do 
+	
+	! Write Final Data
+	open(unit = 1, file = 'benchData.txt')
+	write(1,*) (grid(i,:), i=1,n)
+	close(1)
 end program 
